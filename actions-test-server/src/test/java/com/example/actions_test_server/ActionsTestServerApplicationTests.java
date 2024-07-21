49142eec2d858d1429c18d707c649115c57eca43
HuiJu <leemoon63@gmail.com>
2024-07-21 21:45:10 +0900

    spring project first commit

---
+++ actions-test-server/src/test/java/com/example/actions_test_server/ActionsTestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.actions_test_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionsTestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
